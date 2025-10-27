ed4f9bf79e54ffdcb0e6fddcf71205d469490d92
user <totgor@mail.ru>
2025-10-27 16:23:43 +0300

    Исправил кодировки в jsp и html для русского языка. В jsp cстарая конфигурация была для web.xml, обновил. Для html добавил несколько строк в applicationContextMVC.xml

--- src/main/java/com/totgor/HelloController.java
+++ src/main/java/com/totgor/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     
     @GetMapping("/hello-world")
-    public String printHello() {
+    public String sayHello() {
         return "hello_world";
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,12 @@
 <html>
+
+<head>
+    <meta charset="utf-8">
+    <title>spring-mvc-app1</title>
+</head>
+
 <body>
-<h2>Hello World!</h2>
+    <h2> Корень проекта jsp страница!</h2>
 </body>
+
 </html>
