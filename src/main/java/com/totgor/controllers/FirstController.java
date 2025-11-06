029db99566e288ce496d71cf476c28b668c9e495
user <totgor@mail.ru>
2025-11-06 08:23:12 +0300

    Добавил контроллеры FirstController.java SecondController.java с анотацией @Controller. FirstController возвращает hello.html goodbye.html, SecondController возвращает exit.html. Добавил аннотации @RequestMapping для запуска страниц по запросам first/hello, first/goodbye, second/exit.

--- src/main/java/com/totgor/controllers/FirstController.java
+++ src/main/java/com/totgor/controllers/FirstController.java
@@ -2,9 +2,11 @@ package com.totgor.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
     
     @GetMapping("/hello")
