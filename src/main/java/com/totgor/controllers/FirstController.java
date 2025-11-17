dfaabf5b094749da54815f67e8105c5eabdcd6b7
user <totgor@mail.ru>
2025-11-17 09:56:45 +0300

    Передаем параметры name и sername в http GET запрос. Обрабатываем при помощи аннотации @RequestParam(name), либо если хотим чтобы не выдавал ошибку 400 на отсутствие параметров @RequestParam(value=name, required = false)

--- src/main/java/com/totgor/controllers/FirstController.java
+++ src/main/java/com/totgor/controllers/FirstController.java
@@ -3,8 +3,8 @@ package com.totgor.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import jakarta.servlet.http.HttpServletRequest;
 
 
 @Controller
@@ -12,9 +12,8 @@ import jakarta.servlet.http.HttpServletRequest;
 public class FirstController {
     
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name, 
+                            @RequestParam(value = "surname", required = false) String surname) {
         System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
