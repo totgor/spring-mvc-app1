ff74c497a864ae23bdc737c55c7c57e911bdb74f
user <totgor@mail.ru>
2025-11-18 07:50:09 +0300

    Добавил в метод helloPage, контроллера FirstController параметр типа Model. Spring сам импортирует текущую модель в этот параметр. Далее, с ключом message, передаю строку в которую, в свою очередь, передаю параметры name и surname из GET запроса. Для этого используется метод model.addAttribute(ключ, значение). В представление hello.html добавляю строку <p th:text=></p>. Где th - Themeleaf-шаблонизатор. th:text - можно читать, как подставить текст из модели по ключу .

--- src/main/java/com/totgor/controllers/FirstController.java
+++ src/main/java/com/totgor/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.totgor.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,8 +14,11 @@ public class FirstController {
     
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name, 
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        // System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
